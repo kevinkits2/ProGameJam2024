c68e5c1123933d9251f3667cf14e121a5d12fb4e
Kaur Huko Käämbre <105814740+KaurHuko@users.noreply.github.com>
2024-01-28 14:06:50 +0200

    pressure plate

--- Assets/Scripts/PlayerTeleport.cs
+++ Assets/Scripts/PlayerTeleport.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class PlayerTeleport : MonoBehaviour {
 
-    private enum TimelineState {
+    public static PlayerTeleport Instance;
+
+    public enum TimelineState {
         Past,
         Future
     }
@@ -15,6 +17,15 @@ public class PlayerTeleport : MonoBehaviour {
 
     private TimelineState currentTimelineState = TimelineState.Past;
 
+
+    private void Awake() {
+        if (Instance != null) {
+            Debug.LogError("Player Teleport instance already exists!");
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+    }
 
     private void Start() {
         InputManager.Instance.OnTeleportPerformed += HandleTeleportPerformed;
@@ -39,16 +50,20 @@ public class PlayerTeleport : MonoBehaviour {
     }
 
     private void TeleportToFuture() {
-        transform.position = futurePosition;
         currentTimelineState = TimelineState.Future;
+        transform.position = futurePosition;
 
         TeleportFade.Instance.FadeOut();
     }
 
     private void TeleportToPast() {
-        transform.position = pastPosition;
         currentTimelineState = TimelineState.Past;
+        transform.position = pastPosition;
 
         TeleportFade.Instance.FadeOut();
+    }
+
+    public TimelineState GetCurrentTimelineState() {
+        return currentTimelineState;
     }
 }
